cf1d33017828f07a2119daec018979fe7c954463
Aditya Narayan Rai <anrai0410@gmail.com>
2023-01-09 22:44:05 +0530

    First commit

---
+++ aditya4/src/aditya/age.java
@@ -0,0 +1,17 @@
+package aditya;
+import java.util.*;
+public class age {
+
+	public static void main(String[] args) {
+		Scanner ad=new Scanner(System.in);
+		int age = ad.nextInt();
+        if(age>18){
+		System.out.println("adult");
+	}
+        else{
+		System.out.println("age more than 18");
+	}
+
+	}
+
+}
